899ab66ebfa974ecee1287709b589e78f6ac33d7
timir <pritamborkar@gmail.com>
2015-10-03 12:31:24 +0530

    Added comment system with moderator approval

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
+    
 ]
